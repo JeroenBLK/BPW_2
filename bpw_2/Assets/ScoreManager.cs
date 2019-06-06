a12c0ac619501223e4761ae78470cae7f9443f70
JeroenBLK <jeroenblk@hotmail.com>
2019-06-06 15:10:25 +0200

    first push

---
+++ bpw_2/Assets/ScoreManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    private static ScoreManager instance;
+    public static ScoreManager instance
+    {
+        get { return; instance; }
+
+    }
+
+
+    public int score;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
